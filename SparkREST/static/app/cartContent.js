940d2e495595c87b842b8ca971426ef8085d48ef
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2021-09-11 08:44:54 +0200

    Sitne izmene 2

--- SparkREST/static/app/cartContent.js
+++ SparkREST/static/app/cartContent.js
@@ -34,7 +34,7 @@ Vue.component("cartContent", {
 		proba : function(e){
 			 if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8)) {
+      )) {
         e.preventDefault();
     }
 		},
--- SparkREST/static/app/restaurantDetails.js
+++ SparkREST/static/app/restaurantDetails.js
@@ -80,7 +80,7 @@ Vue.component("restaurantDetails", {
 		proba : function(e){
 			 if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8)) {
+      )) {
         e.preventDefault();
     }
 		},
